e30cc71ff35c799f50f282bcb51d9a8fe02610cb
Manoela Urias <maanoelaurias@gmail.com>
2023-03-30 11:00:39 -0300

    adição de elemento via js

--- main.js
+++ main.js
@@ -3,6 +3,21 @@ const form = document.getElementById("novoItem");
 form.addEventListener ("submit", (event) => {
     event.preventDefault();
 
-    console.log(event)
-    console.log(event.target.elements["nome"].value)
+    criaElemento(event.target.elements["nome"].value, event.target.elements["quantidade"].value)
 })
+
+function criaElemento (nome, quantidade) {
+    
+    const novoItem = document.createElement("li");
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement("strong");
+    numeroItem.innerHTML = quantidade;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    const lista = document.getElementById("lista")
+
+    lista.appendChild(novoItem)
+}
